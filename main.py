cefefc5e44526d8bc19320e227c5609c5ea80ee4
TheKarlos <kamtechie@gmail.com>
2017-11-02 19:02:24 +0530

    PiCam working version

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import json
 from os.path import join, dirname
 from os import environ
-from time import time
+import time
 from watson_developer_cloud import VisualRecognitionV3
 from picamera import PiCamera
 camera = PiCamera()
 camera.resolution = (1024, 768)
-sleep(2)
-file_path = join(dirname(__file__), time() + '.jpg')
+time.sleep(2)
+file_path = join(dirname(__file__), str(time.time()) + '.jpg')
 camera.capture(file_path)
 
-visual_recognition = VisualRecognitionV3('2016-05-20', api_key='145b047be11f5059687578f4ca85325d23e0cdf8')
+visual_recognition = VisualRecognitionV3('2016-05-20', api_key='9f67d912993f80609db8173387e7b32b8d1776c8')
 
 with open(file_path, 'rb') as image_file:
-    text_results = visual_recognition.classify(images_file=image_file,classifier_ids=['WasteType_909361399'])
+    text_results = visual_recognition.classify(images_file=image_file,classifier_ids=['EcoVision_2039938386'],threshold=0.001)
     print(text_results['images'][0]['classifiers'][0]['classes'][0]['class'])
     print('Confidence:',text_results['images'][0]['classifiers'][0]['classes'][0]['score'])
 
